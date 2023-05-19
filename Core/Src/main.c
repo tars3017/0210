adef7671fae4537cf783f0db10d8ebe7cbcdc63b
tars3017 <prgmpclh@gmail.com>
2023-05-19 17:57:34 +0800

    I think it's last time, we use 10 cm.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -147,9 +147,9 @@ int main(void)
 	push_vel_y = 0;
 	push_vel_z = 0;
 
-	lx = 8; // lx, ly not measure yet
-	ly = 20;
-	r = 5; // cm(measure on 5/18)
+	lx = 0.8; // lx, ly not measure yet
+	ly = 2;
+	r = 0.5; // 10 cm(measure on 5/18)
 
 	gear = 75;
   /* USER CODE END 1 */
